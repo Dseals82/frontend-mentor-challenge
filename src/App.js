c8137fa71cc22a5d36f1823c6f536fc5047e0f85
Darius Seals <dariusseals@dariuss-mbp.myfiosgateway.com>
2022-05-09 16:40:59 -0400

    added initial stying for landing page and nav

--- src/App.js
+++ src/App.js
@@ -3,34 +3,45 @@ import { ReactComponent as DataBizLogo } from './assets/images/client-databiz.sv
 import { ReactComponent as AudioPhileLogo } from './assets/images/client-audiophile.svg'
 import { ReactComponent as MeetLogo } from './assets/images/client-meet.svg'
 import { ReactComponent as MakerLogo } from './assets/images/client-maker.svg'
+import { ReactComponent as IconArrowUp } from './assets/images/icon-arrow-up.svg'
+import { ReactComponent as IconArrowDown } from './assets/images/icon-arrow-down.svg'
 import ImageHeroDesktopSrc from './assets/images/image-hero-desktop.png'
 const App = () => {
   return (
     <div className="App">
       <nav className="nav">
-      <Logo />
-        <div className="nav-links-container nav-links-container-left">
-          <a href="/" className="nav-links">Features</a>
-          <a href="/" className="nav-links">Company</a>
-          <a href="/" className="nav-links">Careers</a>
-          <a href="/" className="nav-links">About</a>
+      
+        
+        <div className='nav-links-main-container'>
+        
+          <div className="nav-links-container nav-links-container-left">
+          <Logo className='nav-logo' />
+            <a href="/" className="nav-links grey-color">Features <IconArrowDown /></a>
+            <a href="/" className="nav-links grey-color">Company <IconArrowDown /></a>
+            <a href="/" className="nav-links grey-color">Careers</a>
+            <a href="/" className="nav-links grey-color">About</a>
+          </div>
+        
+          <div className="nav-links-container nav-links-container-right">
+            <a href="/" className="nav-links grey-color ">Login</a>
+            <a href="/" className="nav-links grey-color nav-btn ">Register</a>
+          </div>
         </div>
-        <div className="nav-links-container nav-links-container-right">
-          <a href="/" className="nav-links">Login</a>
-          <a href="/" className="nav-links">Register</a>
-        </div>
+          
+        
+        
       </nav>
     
     
       <main className="main">
         <div className="main-left-container">
-          <h1> Make remote work</h1>
+          <span className='main-h1-make'>Make</span><h1 className='main-h1'>  remote work</h1>
           <div className='btn-p-container'>
-            <p>
+            <p className='main-p'>
               Get your team in sync, no matter your location. Streamline processes, 
               create team rituals, and watch productivity soar.
             </p>
-            <button>Learn more</button>
+            <button className='main-btn'>Learn more</button>
           </div>
           
           <footer className='main-left-container-footer'>
